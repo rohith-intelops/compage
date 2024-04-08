c7a58c67d50ddbdf3a96151ce84239dd063e897b
rohith-intelops <21071a6929@vnrvjiet.in>
2024-04-08 14:40:24 +0530

    go echo server is initiated

---
+++ internal/languages/golang/frameworks/go-echo-server/copier.go
@@ -0,0 +1,402 @@
+package goechoserver
+
+import (
+	"fmt"
+	"html/template"
+
+	"github.com/intelops/compage/internal/languages/executor"
+	"github.com/intelops/compage/internal/languages/golang/frameworks"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/gertd/go-pluralize"
+	"github.com/iancoleman/strcase"
+	corenode "github.com/intelops/compage/internal/core/node"
+	log "github.com/sirupsen/logrus"
+
+	"strings"
+
+	"github.com/intelops/compage/internal/utils"
+)
+
+const EchoServerPath = "/pkg/echo/server"
+const EchoClientPath = "/pkg/echo/client"
+const ConfigPath = "/config"
+
+const ControllersPath = EchoServerPath + "/controllers"
+const ServicesPath = EchoServerPath + "/services"
+const DaosPath = EchoServerPath + "/daos"
+const ModelsPath = EchoServerPath + "/models"
+
+const SQLDBClientsPath = DaosPath + "/clients/sqls"
+const SQLControllerFile = "sqls-controller.go.tmpl"
+const SQLServiceFile = "sqls-service.go.tmpl"
+
+const DaoFile = "dao.go.tmpl"
+
+const SQLGORMModelFile = "sqls-gorm-model.go.tmpl"
+const PostgresGORMDBConfigFile = "postgresql-gorm.go.tmpl"
+const PostgresGORMDaoFile = "postgresql-gorm-dao.go.tmpl"
+
+const ClientFile = "client.go.tmpl"
+const ConfigFile = "echo-opentel-config.go.tmpl"
+
+const PostgreSQLGORM = "PostgreSQL-GORM"
+
+type Copier struct {
+	NodeDirectoryName string
+	TemplatesRootPath string
+	Data              map[string]interface{}
+	IsRestServer      bool
+	HasRestClients    bool
+	SQLDB             string
+	IsSQLDB           bool
+	NoSQLDB           string
+	IsNoSQLDB         bool
+	RestServerPort    string
+	Resources         []*corenode.Resource
+	ResourceConfig    map[string]*frameworks.RestResourceData
+	RestClients       []*corenode.RestClient
+	PluralizeClient   *pluralize.Client
+}
+
+func NewCopier(gitPlatformURL, gitPlatformUserName, gitRepositoryName, nodeName, nodeDirectoryName, templatesRootPath string, isRestServer bool, restServerPort string, isSQLDB bool, sqlDB string, isNoSQLDB bool, noSQLDB string, resources []*corenode.Resource, restClients []*corenode.RestClient) *Copier {
+	var restResourceConfig = make(map[string]*frameworks.RestResourceData)
+	pluralizeClient := pluralize.NewClient()
+
+	// populate map to replace templates
+	data := map[string]interface{}{
+		"GitRepositoryName":   gitRepositoryName,
+		"NodeName":            strings.ToLower(nodeName),
+		"GitPlatformUserName": gitPlatformUserName,
+		"GitPlatformURL":      strings.Replace(gitPlatformURL, "https://", "", -1),
+	}
+	data["SQLDB"] = sqlDB
+	data["IsSQLDB"] = isSQLDB
+	data["NoSQLDB"] = noSQLDB
+	data["IsNoSQLDB"] = isNoSQLDB
+	if isRestServer {
+		var restResourceData []*frameworks.RestResourceData
+		for _, r := range resources {
+			lowerCamelResourceName := strcase.ToLowerCamel(r.Name)
+			resourceData := frameworks.RestResourceData{
+				SmallKebabCaseResourceNameSingular: strcase.ToKebab(r.Name),
+				SmallSnakeCaseResourceNameSingular: strcase.ToSnake(r.Name),
+				SmallResourceNameSingular:          lowerCamelResourceName,
+				SmallResourceNamePlural:            pluralizeClient.Plural(lowerCamelResourceName),
+				CapsResourceNameSingular:           r.Name,
+				CapsResourceNamePlural:             pluralizeClient.Plural(r.Name),
+			}
+			frameworks.AddRESTAllowedMethods(&resourceData, r.AllowedMethods)
+			restResourceConfig[r.Name] = &resourceData
+			restResourceData = append(restResourceData, &resourceData)
+		}
+		data["RestResources"] = restResourceData
+		data["RestServerPort"] = restServerPort
+		data["IsRestServer"] = isRestServer
+	}
+	// if restClients slice has elements
+	hasRestClients := len(restClients) > 0
+
+	return &Copier{
+		TemplatesRootPath: templatesRootPath,
+		NodeDirectoryName: nodeDirectoryName,
+		Data:              data,
+		IsRestServer:      isRestServer,
+		RestServerPort:    restServerPort,
+		HasRestClients:    hasRestClients,
+		SQLDB:             sqlDB,
+		IsSQLDB:           isSQLDB,
+		NoSQLDB:           noSQLDB,
+		IsNoSQLDB:         isNoSQLDB,
+		Resources:         resources,
+		ResourceConfig:    restResourceConfig,
+		RestClients:       restClients,
+		PluralizeClient:   pluralizeClient,
+	}
+}
+
+// createRestClientDirectories creates rest client directories.
+func (c *Copier) createRestClientDirectories() error {
+	clientDirectory := c.NodeDirectoryName + EchoClientPath
+	if err := utils.CreateDirectories(clientDirectory); err != nil {
+		log.Errorf("error creating client directory: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+// createRestServerDirectories creates rest server directories.
+func (c *Copier) createRestServerDirectories() error {
+	configDirectory := c.NodeDirectoryName + ConfigPath
+	controllersDirectory := c.NodeDirectoryName + ControllersPath
+	modelsDirectory := c.NodeDirectoryName + ModelsPath
+	servicesDirectory := c.NodeDirectoryName + ServicesPath
+	daosDirectory := c.NodeDirectoryName + DaosPath
+	if err := utils.CreateDirectories(configDirectory); err != nil {
+		log.Errorf("error creating config directory: %v", err)
+		return err
+	}
+	if err := utils.CreateDirectories(controllersDirectory); err != nil {
+		log.Errorf("error creating controllers directory: %v", err)
+		return err
+	}
+	if err := utils.CreateDirectories(modelsDirectory); err != nil {
+		log.Errorf("error creating models directory: %v", err)
+		return err
+	}
+	if err := utils.CreateDirectories(servicesDirectory); err != nil {
+		log.Errorf("error creating services directory: %v", err)
+		return err
+	}
+	if err := utils.CreateDirectories(daosDirectory); err != nil {
+		log.Errorf("error creating daos directory: %v", err)
+		return err
+	}
+	if c.IsSQLDB {
+		// create directories for every resource's db client.
+		sqlDBClientsDirectory := c.NodeDirectoryName + SQLDBClientsPath
+		if err := utils.CreateDirectories(sqlDBClientsDirectory); err != nil {
+			log.Errorf("error creating sql db clients directory: %v", err)
+			return err
+		}
+	}
+	return nil
+}
+
+// copyRestServerResourceFiles copies rest server resource files from template and renames them as per resource config.
+func (c *Copier) copyRestServerResourceFiles(resource *corenode.Resource) error {
+	var filePaths []*string
+	var err error
+	resourceName := strcase.ToKebab(resource.Name)
+
+	// copy sql controller/service/dao/models files to a generated project
+	if c.IsSQLDB {
+		filePaths, err = c.copySQLDBResourceFiles(resourceName, filePaths)
+		if err != nil {
+			log.Errorf("error copying sql db resources: %v", err)
+			return err
+		}
+	}
+	// add resource-specific data to map in c needed for templates.
+	err = c.addResourceSpecificTemplateData(resource)
+	if err != nil {
+		log.Errorf("error adding resource specific template data: %v", err)
+		return err
+	}
+
+	// get func map for template
+	funcMap := c.getFuncMap(resource)
+
+	// apply template
+	return executor.ExecuteWithFuncs(filePaths, c.Data, funcMap)
+}
+
+func (c *Copier) getFuncMap(resource *corenode.Resource) template.FuncMap {
+	funcMap := template.FuncMap{
+		"ToLowerCamelCase": func(key string) string {
+			return strcase.ToLowerCamel(key)
+		},
+		"AddPointerIfCompositeField": func(key string) string {
+			fieldMetaData, ok := resource.Fields[key]
+			if ok && fieldMetaData.IsComposite {
+				return "*" + fieldMetaData.Type
+			}
+			return fieldMetaData.Type
+		},
+		// This function helps the template to add a foreignKey based on composite field
+		"AddForeignKeyIfCompositeField": func(key, value string) string {
+			if fieldMetaData, ok := resource.Fields[key]; ok && fieldMetaData.IsComposite {
+				return fmt.Sprintf("%s %s `gorm:\"foreignKey:ID\" json:\"%s,omitempty\"`", key, value, strcase.ToLowerCamel(key))
+			}
+			return fmt.Sprintf("%s %s `json:\"%s,omitempty\"`", key, value, strcase.ToLowerCamel(key))
+		},
+		"GetCompositeFields": func(key string) string {
+			fieldMetaData, ok := resource.Fields[key]
+			if ok && fieldMetaData.IsComposite {
+				return key + ": &models." + fieldMetaData.Type + "{},"
+			}
+			return ""
+		},
+	}
+	return funcMap
+}
+
+// copyRestClientResourceFiles copies rest client files from template and renames them as per client config.
+func (c *Copier) copyEchoClientResourceFiles(restClient *corenode.RestClient) error {
+	/// add resource-specific data to map in c needed for templates.
+	c.Data["RestClientPort"] = restClient.Port
+	c.Data["RestClientServiceName"] = restClient.SourceNodeName
+	c.Data["RestClientSourceNodeID"] = strings.Replace(cases.Title(language.Und, cases.NoLower).String(restClient.SourceNodeID), "-", "", -1)
+
+	// copy restClient files to a generated project.
+	targetResourceClientFileName := c.NodeDirectoryName + EchoClientPath + "/" + restClient.SourceNodeName + "-" + ClientFile
+	_, err := utils.CopyFile(targetResourceClientFileName, c.TemplatesRootPath+EchoClientPath+"/"+ClientFile)
+	if err != nil {
+		log.Errorf("error while copying rest client file %s", targetResourceClientFileName)
+		return err
+	}
+	var filePaths []*string
+	filePaths = append(filePaths, &targetResourceClientFileName)
+
+	// apply template
+	return executor.Execute(filePaths, c.Data)
+}
+
+func (c *Copier) addResourceSpecificTemplateData(resource *corenode.Resource) error {
+	// make every field public by making its first character capital.
+	fields := map[string]string{}
+	for key, value := range resource.Fields {
+		fields[key] = value.Type
+	}
+	c.Data["Fields"] = fields
+
+	// Add another map with the below keys at root level (this is for specific resource for this iteration)
+	restResourceData := c.ResourceConfig[resource.Name]
+	c.Data["SmallKebabCaseResourceNameSingular"] = restResourceData.SmallKebabCaseResourceNameSingular
+	c.Data["SmallSnakeCaseResourceNameSingular"] = restResourceData.SmallSnakeCaseResourceNameSingular
+	c.Data["SmallResourceNameSingular"] = restResourceData.SmallResourceNameSingular
+	c.Data["SmallResourceNamePlural"] = restResourceData.SmallResourceNamePlural
+	c.Data["CapsResourceNameSingular"] = restResourceData.CapsResourceNameSingular
+	c.Data["CapsResourceNamePlural"] = restResourceData.CapsResourceNamePlural
+	c.Data["IsRESTCreateAllowed"] = restResourceData.IsRESTCreateAllowed
+	c.Data["IsRESTListAllowed"] = restResourceData.IsRESTListAllowed
+	c.Data["IsRESTGetAllowed"] = restResourceData.IsRESTGetAllowed
+	c.Data["IsRESTPutAllowed"] = restResourceData.IsRESTPutAllowed
+	c.Data["IsRESTDeleteAllowed"] = restResourceData.IsRESTDeleteAllowed
+	c.Data["IsRESTPatchAllowed"] = restResourceData.IsRESTPatchAllowed
+	c.Data["IsRESTOptionsAllowed"] = restResourceData.IsRESTPatchAllowed
+	c.Data["IsRESTHeadAllowed"] = restResourceData.IsRESTHeadAllowed
+
+	return nil
+}
+
+func (c *Copier) copySQLDBResourceFiles(resourceName string, filePaths []*string) ([]*string, error) {
+	// copy controller files to a generated project
+	targetResourceControllerFileName := c.NodeDirectoryName + ControllersPath + "/" + resourceName + "-" + strings.Replace(SQLControllerFile, "sqls-", "", 1)
+	_, err := utils.CopyFile(targetResourceControllerFileName, c.TemplatesRootPath+ControllersPath+"/"+SQLControllerFile)
+	if err != nil {
+		log.Debugf("error copying controller file: %v", err)
+		return nil, err
+	}
+	filePaths = append(filePaths, &targetResourceControllerFileName)
+
+	// copy service files to a generated project
+	targetResourceServiceFileName := c.NodeDirectoryName + ServicesPath + "/" + resourceName + "-" + strings.Replace(SQLServiceFile, "sqls-", "", 1)
+	_, err = utils.CopyFile(targetResourceServiceFileName, c.TemplatesRootPath+ServicesPath+"/"+SQLServiceFile)
+	if err != nil {
+		log.Debugf("error copying service file: %v", err)
+		return nil, err
+	}
+	filePaths = append(filePaths, &targetResourceServiceFileName)
+
+	var targetResourceDaoFileName string
+	if c.SQLDB == PostgreSQLGORM {
+		// model files
+		targetResourceModelFileName := c.NodeDirectoryName + ModelsPath + "/" + resourceName + "-" + strings.Replace(SQLGORMModelFile, "sqls-gorm-", "", 1)
+		_, err = utils.CopyFile(targetResourceModelFileName, c.TemplatesRootPath+ModelsPath+"/"+SQLGORMModelFile)
+		if err != nil {
+			log.Debugf("error copying postgresql model file: %v", err)
+			return nil, err
+		}
+		filePaths = append(filePaths, &targetResourceModelFileName)
+
+		// dao files
+		targetResourceDaoFileName = c.NodeDirectoryName + DaosPath + "/" + resourceName + "-" + strings.Replace(PostgresGORMDaoFile, "postgresql-gorm-", "", 1)
+		_, err = utils.CopyFile(targetResourceDaoFileName, c.TemplatesRootPath+DaosPath+"/"+PostgresGORMDaoFile)
+		if err != nil {
+			log.Debugf("error copying postgresql dao file: %v", err)
+			return nil, err
+		}
+		filePaths = append(filePaths, &targetResourceDaoFileName)
+	}
+	return filePaths, nil
+}
+
+func (c *Copier) CreateRestServer() error {
+	// if the node is server, add server code
+	if c.IsRestServer {
+		// create directories for controller, service, dao, models
+		if err := c.createRestServerDirectories(); err != nil {
+			log.Errorf("error creating rest server directories: %v", err)
+			return err
+		}
+		// copy files with respect to the names of resources
+		for _, resource := range c.Resources {
+			if err := c.copyRestServerResourceFiles(resource); err != nil {
+				log.Errorf("error copying rest server resource files: %v", err)
+				return err
+			}
+		}
+		// copy open telemetry config file
+		var filePaths []*string
+		// client files
+		targetOpenTelConfigFileName := c.NodeDirectoryName + ConfigPath + "/" + ConfigFile
+		_, err := utils.CopyFile(targetOpenTelConfigFileName, c.TemplatesRootPath+ConfigPath+"/"+ConfigFile)
+		if err != nil {
+			log.Debugf("error copying opentel config file: %v", err)
+			return err
+		}
+		filePaths = append(filePaths, &targetOpenTelConfigFileName)
+		err = executor.Execute(filePaths, c.Data)
+		if err != nil {
+			log.Debugf("error executing opentel config file: %v", err)
+			return err
+		}
+
+		if c.IsSQLDB {
+			// create sql db config file (common to all resources for specific database)
+			// No vars in config file as of now, but in future they may be there.
+			if c.SQLDB == PostgreSQLGORM {
+				filePaths = []*string{}
+				// client files
+				targetMapConfigFileName := c.NodeDirectoryName + SQLDBClientsPath + "/" + PostgresGORMDBConfigFile
+				_, err = utils.CopyFile(targetMapConfigFileName, c.TemplatesRootPath+SQLDBClientsPath+"/"+PostgresGORMDBConfigFile)
+				if err != nil {
+					log.Debugf("error copying map config file: %v", err)
+					return err
+				}
+				filePaths = append(filePaths, &targetMapConfigFileName)
+				return executor.Execute(filePaths, c.Data)
+			}
+		}
+	}
+	return nil
+}
+
+// CreateRestClients creates/copies relevant files to a generated project
+func (c *Copier) CreateRestClients() error {
+	// if the node is client, add client code
+	if c.HasRestClients {
+		// create directories for a client
+		if err := c.createRestClientDirectories(); err != nil {
+			log.Errorf("error creating rest client directories: %v", err)
+			return err
+		}
+
+		// copy files with respect to the names of resources
+		for _, client := range c.RestClients {
+			if err := c.copyEchoClientResourceFiles(client); err != nil {
+				log.Errorf("error copying rest client resource files: %v", err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// CreateRootLevelFiles copies all root level files at language template.
+func (c *Copier) CreateRootLevelFiles() error {
+	err := utils.CopyFiles(c.NodeDirectoryName, c.TemplatesRootPath)
+	if err != nil {
+		log.Errorf("error copying root level files: %v", err)
+		return err
+	}
+	_, files, err0 := utils.GetDirectoriesAndFilePaths(c.NodeDirectoryName)
+	if err0 != nil {
+		log.Errorf("error getting directories and file paths: %v", err0)
+		return err0
+	}
+	return executor.Execute(files, c.Data)
+}
